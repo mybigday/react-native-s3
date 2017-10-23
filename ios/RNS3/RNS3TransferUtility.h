35820f84130fb14065add23ca5d3c16a6d8b9ab1
森田 昌宏 <m.morita@getgamba.com>
2017-10-23 16:31:22 +0900

    RN0.49 compatibility

--- ios/RNS3/RNS3TransferUtility.h
+++ ios/RNS3/RNS3TransferUtility.h
@@ -1,11 +1,6 @@
 // import RCTBridgeModule.h
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#import "RCTEventDispatcher.h"
-#else
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventDispatcher.h>
-#endif
 
 #import <AWSCore/AWSCore.h>
 #import <AWSS3/AWSS3.h>
