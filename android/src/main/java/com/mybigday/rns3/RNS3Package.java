7195b0ed10f810a3a1712524e35d340143991f85
Olivier STURBOIS <oliviersturbois@gmail.com>
2017-11-14 14:20:52 +0100

    comment Override for supporting RN > 0.47

--- android/src/main/java/com/mybigday/rns3/RNS3Package.java
+++ android/src/main/java/com/mybigday/rns3/RNS3Package.java
@@ -18,7 +18,7 @@ public class RNS3Package implements ReactPackage {
     return modules;
   }
 
-  @Override
+  // @Override
   public List<Class<? extends JavaScriptModule>> createJSModules() {
     return Collections.emptyList();
   }
